fa275e61ce6843842f49398540417a32584f2959
Cody <cody.wherry@gmail.com>
2018-01-28 20:55:49 -0500

    Added Tree Health and Ability to destroy tree

---
+++ Assets/Scripts/AI/BasicBanditAI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBanditAI : MonoBehaviour {
+
+
+    public float health = 100f;
+    public float hunger = 100f;
+    public float thirst = 100f;
+
+    public float hungerSpeedMultiplier = 2.5f;
+    public float thirstSpeedMultiplier = 2.5f;
+    public float healthSpeedMultiplier = 5f;
+
+    public void start()
+    {
+
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Update()
+    {
+        checkHealth();
+    }
+
+    /// <summary>
+    /// Checks the health of the bandit,
+    /// sets bandit's properties appropriately
+    /// </summary>
+    public void checkHealth()
+    {
+        if (health <= 0)
+        {
+            Debug.Log("Bandit died");
+
+            // set bandit ai to false, if bandit is found
+            //if (transform.Find("BanditAI") != null)
+            //{
+                transform.Find("BanditAI").gameObject.SetActive(false);
+            //}
+            //else
+            //{
+            //    Debug.Log("Cannot find bandit");
+            //}
+            // set bandit kinematic to false
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/TreeHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeHealth : MonoBehaviour {
+
+    public int health = 100;
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
