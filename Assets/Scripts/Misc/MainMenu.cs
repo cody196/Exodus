2474572918b8d944dddae80fe9db1180618032e8
Cody <cody.wherry@gmail.com>
2018-01-30 00:48:43 -0500

    Main Menu is fixed

---
+++ Assets/Scripts/Misc/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public void LoadLevel(int level)
+    {
+        Debug.Log("Loading game...");
+        Application.LoadLevel(level);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit Game...");
+        Application.Quit();
+    }
+
+}
